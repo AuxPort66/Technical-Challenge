08c7dbce26e1d65543ce97c8ec13c1f35ab4f9b3
Sara <sara.izquierdo.elfau@gmail.com>
2022-01-24 10:51:55 +0100

    Technical Challenge Up

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    private Dictionary<string,string> solutions;
+
+    [System.Serializable]
+    public class Solution
+    {
+        public string answer;
+        public string question;
+    }
+
+    [System.Serializable]
+    public class SolutionsData
+    {
+        public Solution[] solutionsdata;
+    }
+
+    [Header("Data")]
+    [SerializeField] private TextAsset jsonData;
+
+    [Header("Managers")]
+    [SerializeField] private UIManager UImanager;
+
+    [Header("Settings")]
+    [SerializeField] private int numAnswers = 3;
+    [SerializeField] private float questionSecondsOnScreen = 2f;
+    [SerializeField] private float fadeSeconds = 2f;
+
+    private string actualsolution;
+
+    private int errors;
+    private int success;
+
+    private int roundErrors;
+
+    public void InputPlayer(Text answer)
+    {
+        if (answer.text == actualsolution)
+        {
+            AddSuccess();
+            UImanager.MarkCorrect(answer.transform.parent.gameObject);
+            FinishRound();
+        }
+        else
+        {
+            if (roundErrors == 0)
+            {
+                roundErrors++;
+                AddError();
+                UImanager.MarkIncorrect(answer.transform.parent.gameObject);
+                StartCoroutine(UImanager.FadeOutOneAnswer_CO(answer.transform.parent.gameObject, fadeSeconds));
+            }
+            else
+            {
+                AddError();
+                UImanager.MarkSolution(actualsolution);
+                FinishRound();
+            }
+        }
+
+    }
+
+    void Awake()
+    {
+        ChargeSolutionsData();
+        Initialization();
+    }
+
+    private void ChargeSolutionsData()
+    {
+        solutions = new Dictionary<string, string>();
+        var data = JsonUtility.FromJson<SolutionsData>(jsonData.text);
+        foreach(Solution solution in data.solutionsdata)
+        {
+            solutions.Add(solution.answer, solution.question);
+        }
+    }
+
+    private void Initialization()
+    {
+        errors = 0;
+        success = 0;
+        roundErrors = 0;
+
+        UImanager.SetErrors(errors);
+        UImanager.SetSucces(success);
+    }
+
+    void Start()
+    {
+        StartCoroutine(CreateQuestion_CO());
+    }
+
+    private IEnumerator CreateQuestion_CO()
+    {
+        SelectQuestion();
+        UImanager.SetQuestionText(solutions[actualsolution]);
+        yield return StartCoroutine(UImanager.AnimationQuestion_CO(fadeSeconds, questionSecondsOnScreen));
+        AnswerList();
+    }
+
+    private void SelectQuestion()
+    {
+        var selectsolution = Random.Range(0, solutions.Count);
+        actualsolution = solutions.ElementAt(selectsolution).Key;
+    }
+
+    private void AnswerList()
+    {
+        List<string> optionlist = CreateAnswerList();
+        UImanager.CreateAnswersButtons(optionlist);
+        StartCoroutine(UImanager.FadeInAnswersGrid_CO(fadeSeconds));
+    }
+
+    private List<string> CreateAnswerList()
+    {
+        var keylist = new List<string>(solutions.Keys);
+        keylist.Remove(actualsolution);
+
+        var optionlist = new List<string>();
+
+        for (int i = 0; i < numAnswers; i++)
+        {
+            var randomanswer = Random.Range(0, keylist.Count);
+            optionlist.Add(keylist[randomanswer]);
+            keylist.RemoveAt(randomanswer);
+        }
+
+        var randomindex = Random.Range(0, numAnswers);
+        optionlist[randomindex] = actualsolution;
+
+        return optionlist;
+    }
+
+    private void AddSuccess()
+    {
+        ++success;
+        UImanager.SetSucces(success);
+    }
+
+    private void AddError()
+    {
+        ++errors;
+        UImanager.SetErrors(errors);
+    }
+
+    private void FinishRound()
+    {
+        StartCoroutine(NextRound_CO());
+    }
+
+    private IEnumerator NextRound_CO()
+    {
+        yield return StartCoroutine(UImanager.FadeOutAnswersGrid_CO(fadeSeconds));
+        UImanager.CleanButtons();
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        roundErrors = 0;
+        StartCoroutine(CreateQuestion_CO());
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Managers")]
+    [SerializeField] GameManager gameManager;
+
+    [Header("Components")]
+    [SerializeField] Text question;
+    [SerializeField] GameObject answersGrid;
+    [SerializeField] GameObject answerPrefab;
+    [SerializeField] Text errorsText;
+    [SerializeField] Text successText;
+
+    CanvasGroup canvasgroupAnswers;
+
+    private void Awake()
+    {
+        question.CrossFadeAlpha(0, 0, false);
+        canvasgroupAnswers = answersGrid.GetComponent<CanvasGroup>();
+        canvasgroupAnswers.alpha = 0;
+    }
+
+    public void SetQuestionText(string selectedQuestion)
+    {
+        question.text = selectedQuestion;
+    }
+
+    public IEnumerator AnimationQuestion_CO(float fadeSeconds, float SecondsOnScreen)
+    {
+        FadeInQuestion(fadeSeconds);
+        yield return new WaitForSeconds(fadeSeconds);
+        yield return new WaitForSeconds(SecondsOnScreen);
+        FadeoutQuestion(fadeSeconds);
+        yield return new WaitForSeconds(fadeSeconds);
+    }
+
+    public void FadeInQuestion(float fadeSeconds)
+    {
+        question.CrossFadeAlpha(1, fadeSeconds, false);
+    }
+
+    public void FadeoutQuestion(float fadeSeconds)
+    {
+        question.CrossFadeAlpha(0, fadeSeconds, false);
+    }
+
+    public void CreateAnswersButtons(List<string> listAnswers)
+    {
+        for(int i = 0; i < listAnswers.Count; i++)
+        {
+            GameObject answerButton = Instantiate(answerPrefab, answersGrid.transform, true);
+            var textButton = answerButton.transform.GetChild(0).GetComponent<Text>();
+            textButton.text = listAnswers[i];
+            answerButton.GetComponent<Button>().onClick.AddListener(() => InputClick(textButton));
+        }
+
+        DisableAnswerButtons();
+    }
+
+    public void EnableAnswerButtons()
+    {
+        canvasgroupAnswers.blocksRaycasts = true;
+    }
+    public void DisableAnswerButtons()
+    {
+        canvasgroupAnswers.blocksRaycasts = false;
+    }
+
+    public IEnumerator FadeInAnswersGrid_CO(float fadeSeconds)
+    {
+        for (float i = 0f; i <= fadeSeconds; i += Time.deltaTime)
+        {
+            canvasgroupAnswers.alpha = i;
+            yield return null;
+        }
+        canvasgroupAnswers.alpha = 1f;
+
+        EnableAnswerButtons();
+    }
+
+    public IEnumerator FadeOutAnswersGrid_CO(float fadeSeconds)
+    {
+        DisableAnswerButtons();
+
+        for (float i = fadeSeconds; i >= 0f; i -= Time.deltaTime)
+        {
+            canvasgroupAnswers.alpha = i;
+            yield return null;
+        }
+        canvasgroupAnswers.alpha = 0f;
+    }
+
+    public IEnumerator FadeOutOneAnswer_CO(GameObject answer, float fadeSeconds)
+    {
+        DisableAnswerButtons();
+
+        var canvasgroupAnswer = answer.GetComponent<CanvasGroup>();
+        for (float i = fadeSeconds; i >= 0f; i -= Time.deltaTime)
+        {
+            canvasgroupAnswer.alpha = i;
+            yield return null;
+        }
+        canvasgroupAnswer.alpha = 0f;
+
+        EnableAnswerButtons();
+    }
+
+    public void MarkSolution(string solution)
+    {
+        foreach(Transform answerButton in answersGrid.transform)
+        {
+            var answerText = answerButton.GetChild(0).GetComponent<Text>();
+            if (solution == answerText.text) MarkCorrect(answerButton.gameObject);
+            else MarkIncorrect(answerButton.gameObject);
+        }
+    }
+
+    public void MarkCorrect(GameObject answerButton)
+    {
+        answerButton.GetComponent<Image>().color = Color.green;
+    }
+
+    public void MarkIncorrect(GameObject answerButton)
+    {
+        answerButton.GetComponent<Image>().color = Color.red;
+    }
+
+    public void SetSucces(int success)
+    {
+        successText.text = success.ToString();
+    }
+
+    public void SetErrors(int errors)
+    {
+        errorsText.text = errors.ToString();
+    }
+
+    public void CleanButtons()
+    {
+        foreach (Transform answerButton in answersGrid.transform)
+        {
+            Destroy(answerButton.gameObject);
+        }
+    }
+    public void InputClick(Text answerText)
+    {
+        gameManager.InputPlayer(answerText);
+    }
+
+}
